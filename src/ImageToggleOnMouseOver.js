606500ba9fe7319dc15317181c81071eec258bda
Annie-Huang <annie.hn@gmail.com>
2019-07-20 12:31:43 +1000

    Using useRef to Enable Mouseover to Colorize an Image

--- src/ImageToggleOnMouseOver.js
+++ src/ImageToggleOnMouseOver.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 const ImageToggleOnMouseOver = ({primaryImg, secondaryImg}) => {
+    const imageRef = useRef(null);
+
     return (
-        <img src={primaryImg} alt="" />
+        <img
+            onMouseOver={() => {
+                imageRef.current.src = secondaryImg;
+            }}
+            onMouseOut={() => {
+                imageRef.current.src = primaryImg;
+            }}
+            src={primaryImg}
+            alt=""
+            ref={imageRef}
+        />
     );
 };
 
